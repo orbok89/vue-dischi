159811d2d2364666558c0645b60b4dc8b08ab11b
BigFelk <fedferrara89@gmail.com>
2021-09-22 15:56:33 +0200

    logica finita

---
+++ src/components/Copertina.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-6 col-md-3 col-lg-1 m-5 p-2 prova">
+      <img :src="info.poster" alt="">
+      <div>{{info.title}}</div>
+      <div>{{info.author}}</div>
+      <div>{{info.year}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Copertina',
+    props: ['info']
+}
+</script>
+
+<style  scoped lang="scss">
+@import '~bootstrap/scss/bootstrap';
+    img{
+        width: 100px;
+    }
+    .prova{
+    background-color: darkolivegreen;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
-      prova prova
+     <div class="container">
+         <div class="row text-center  p-3 m-5 ">
+             <Copertina v-for="(disco, index) in array_dischi " :key="index" :info="disco"/>
+         </div>
+     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Copertina from './Copertina.vue';
+
+
 export default {
-    name:'Main'
+    name:'Main',
+    components:{
+        Copertina
+    },
+    data(){
+        return{
+            url_dishi:'https://flynn.boolean.careers/exercises/api/array/music',
+            array_dischi:[]
+        }
+    },
+    created() {
+        this.get_disk();
+
+    },
+    methods:{
+        get_disk(){
+            axios.get(this.url_dishi).then(res =>
+            this.array_dischi=res.data.response
+            )
+        }
+    }
+
 }
 </script>
 
-<style>
+<style  scoped lang="scss">
+@import '~bootstrap/scss/bootstrap';
 
 </style>
